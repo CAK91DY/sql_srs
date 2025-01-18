89f125e60855adde06bcd13754f1235f79c1ceff
cak91dy <cakpochristophes91@gmail.com>
2025-01-18 20:56:31 +0100

    correction du code

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# pylint: disable=missing-module-docstring
 import streamlit as st
 import pandas as pd
 import duckdb
@@ -13,17 +14,21 @@ client = pd.DataFrame(client)
 # Table 2 : Exemple de données des commands
 commands = {
     "order_id": [101, 102, 103],
-    "client_id": [1, 2, 4],  # Remarque : le client_id 4 ne correspond pas à la table df1
+    "client_id": [
+        1,
+        2,
+        4,
+    ],  # Remarque : le client_id 4 ne correspond pas à la table df1
     "amount": [250, 150, 300],
 }
 commands = pd.DataFrame(commands)
 
-answer = ("""
-    SELECT * FROM client l
-    JOIN commands c
-    ON l.client_id = c.client_id
-""")
-solution = duckdb.sql(answer).df()
+ANSWER_STR = """
+    SELECT * FROM client 
+    JOIN commands 
+    USING(client_id)
+"""
+solution_df = duckdb.sql(ANSWER_STR).df()
 
 
 with st.sidebar:
@@ -42,6 +47,20 @@ if query:
     result = duckdb.sql(query).df()
     st.dataframe(result)
 
+    if len(result.columns) != len(solution_df.columns):
+        st.write("Some columns are missing")
+    try:
+        result = result[solution_df.columns]
+        st.dataframe(result.compare(solution_df))
+    except KeyError as e:
+        st.write("Some columns are missing")
+    n_lines_difference = result.shape[0] - solution_df.shape[0]
+    if n_lines_difference != 0:
+        st.write(
+            f"result has a {n_lines_difference} lines difference with the solution"
+        )
+
+
 tab1, tab2 = st.tabs(["Tables", "Solution"])
 with tab1:
     st.write("table: clients")
@@ -49,9 +68,8 @@ with tab1:
     st.write("table: commands")
     st.dataframe(commands)
     st.write("expected:")
-    st.dataframe(solution)
+    st.dataframe(solution_df)
 
 
 with tab2:
-    st.write(answer)
-
+    st.write(ANSWER_STR)
